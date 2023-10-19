3ce2c1cc0846b5b3f5efcf240afaa356f5b5e754
angeerik17 <diraboua2@gmail.com>
2023-10-19 18:31:07 +0000

    maj1

--- lib/home.dart
+++ lib/home.dart
@@ -78,51 +78,55 @@ class Home extends StatelessWidget {
           ),
           const SizedBox(height: 5),
           buildImagesInPairs(),
-          Center(
-            child: Container(
-              margin: const EdgeInsets.only(bottom: 16.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                gradient: const LinearGradient(
-                  colors: [Colors.yellow, Colors.blue],
-                ),
+        ],
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(bottom: 16.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              gradient: const LinearGradient(
+                colors: [Colors.yellow, Colors.blue],
               ),
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(50),
-                  onTap: () {
-                    // Action à effectuer lorsque le bouton est cliqué
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 19,
-                      horizontal: 40,
-                    ),
-                    color: Colors
-                        .transparent, // Définir la couleur d'arrière-plan à transparent
-                    child: const Text(
-                      'Map View',
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: BorderRadius.circular(50),
+                onTap: () {
+                  // Action à effectuer lorsque le bouton est cliqué
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 19,
+                    horizontal: 40,
+                  ),
+                  color: Colors.transparent,
+                  child: const Text(
+                    'Map View',
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
                   ),
                 ),
               ),
             ),
           ),
+          const SizedBox(height: 100),
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: ClipRRect(
         borderRadius: const BorderRadius.only(
           topLeft: Radius.circular(50.0),
           topRight: Radius.circular(50.0),
         ),
         child: BottomAppBar(
-          height: 100,
+          shape: const CircularNotchedRectangle(),
           color: Colors.white,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -196,12 +200,12 @@ class Home extends StatelessWidget {
                     child: IconButton(
                       icon: const Icon(Icons.bookmark_border_outlined),
                       onPressed: () {
-                        // Action à effectuer lorsqu'on appuie sur l'icône de sauvegarde
+                        // Action à effectuer lorsqu'on appuie sur l'icône de la boîte de réception
                       },
                     ),
                   ),
                   const Text(
-                    "Save",
+                    "Inbox",
                     style: TextStyle(
                       fontSize: 17,
                       fontWeight: FontWeight.bold,
